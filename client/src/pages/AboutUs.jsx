1b9c79eb27a43ba180f453311edb6562e4aedd50
Doodh Nath Tiwari <doodhnathtiwari7@gmail.com>
2024-03-13 08:13:24 +0530

    updates about us page and homepage

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,13 +1,260 @@
-import React from 'react'
-import iitrpr from '/iitrpr.jpg'
+import React, { useState } from "react";
+import iitrpr from "/iitrpr.jpg";
+import "./about.css";
 
 const AboutUs = () => {
-  return (
-    <div className='flex flex-col items-center h-screen mt-24 bg-cover bg-center' style={{ backgroundImage: 'url(${iitrpr})'}}>
-      <h1 className='text-9xl text-black'>About Us</h1>
-      <p className='`text-7xl text-black mt-5 mb-5'>This portal is designed to shift the current on-paper process to a complete online mode.</p>
-    </div>
-  )
-}
+  const [isActive, setIsActive] = useState(false);
+  const [isActive1, setIsActive1] = useState(false);
+  const [isActive2, setIsActive2] = useState(false);
+  const [isActive3, setIsActive3] = useState(false);
+  
 
-export default AboutUs
+  const toggleActive = () => {
+    setIsActive(!isActive);
+  };
+  const toggleActive1 = () => {
+    setIsActive1(!isActive1);
+  };
+
+  const toggleActive2 = () => {
+    setIsActive2(!isActive2);
+  };
+
+  const toggleActive3 = () => {
+    setIsActive3(!isActive3);
+  };
+
+  return (
+
+
+    <div className="home1 ">
+    <h2 className="text-6xl text-center text-black mt-14 mb-0">Teams</h2>
+    <div className="flex flex-col justify-center  bg-cover bg-center">
+      <div>
+        <div className="flex flex-row">
+        <div id="card">
+          <div id="blur">
+            <div id="color"></div>
+          </div>
+          <div id="profile">
+            <img
+              src="Doodhnath.jpeg"
+              alt="Doodh Nath"
+            />
+            <h1>Doodh Nath Tiwari</h1>
+            <p>+91 6203087513</p>
+            <p>Front-end Web Developer</p>
+            <div className={`info ${isActive ? "active" : ""}`}>
+              <i className="fa fa-info fa-1x block" onClick={toggleActive}></i>
+              <i
+                className="fa fa-angle-down fa-2x block"
+                onClick={toggleActive}
+              ></i>
+              <p>
+                I'm a  enthusiastic web developer and 
+                Currently Pursuing my B Tech @{" "}
+               <a href="https://www.iitrpr.ac.in/" target="_blank">
+                  IIT Ropar
+                </a>
+                .
+              </p>
+              <p>Find me on:</p>
+              <a
+                className="link"
+                href="https://twitter.com/doodhnath1/"
+                target="_blank"
+              >
+                <i className="fa fa-twitter fa-2x social"></i>
+              </a>
+              <a
+                className="link"
+                href="http://dribbble.com/doodhnath1/"
+                target="_blank"
+              >
+                <i className="fa fa-dribbble fa-2x social"></i>
+              </a>
+              <a
+                className="link"
+                href="https://codepen.io/doodhnath1"
+                target="_blank"
+              >
+                <i className="fa fa-codepen fa-2x social"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div id="card">
+          <div id="blur">
+            <div id="color"></div>
+          </div>
+          <div id="profile">
+            <img
+              src="Divyankar.jpeg"
+              alt="User"
+            />
+            <h1>Divyankar Shah</h1>
+            <p>+91 9569691304</p>
+            <p>Front-end Web Developer</p>
+            <div className={`info ${isActive1 ? "active" : ""}`}>
+              <i className="fa fa-info fa-1x block" onClick={toggleActive1}></i>
+              <i
+                className="fa fa-angle-down fa-2x block"
+                onClick={toggleActive1}
+              ></i>
+              <p>
+                In short, I spend my earthly time striving to create some darned
+                awesome UI designs, and also develop a few. Student @{" "}
+               <a href="https://www.iitrpr.ac.in/" target="_blank">IIT Ropar
+                </a>
+                .
+              </p>
+              <p>Find me on:</p>
+              <a
+                className="link"
+                href="https://twitter.com/Masquetina/"
+                target="_blank"
+              >
+                <i className="fa fa-twitter fa-2x social"></i>
+              </a>
+              <a
+                className="link"
+                href="http://dribbble.com/Masquetina/"
+                target="_blank"
+              >
+                <i className="fa fa-dribbble fa-2x social"></i>
+              </a>
+              <a
+                className="link"
+                href="https://codepen.io/Masquetina"
+                target="_blank"
+              >
+                <i className="fa fa-codepen fa-2x social"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+        </div>
+
+
+
+
+        <div className="flex flex-row">
+        <div id="card">
+          <div id="blur">
+            <div id="color"></div>
+          </div>
+          <div id="profile">
+            <img
+              src="Niroopma.jpg"
+              alt="User"
+            />
+            <h1>Niroopma Verma</h1>
+            <p> +91 7705834401</p>
+            <p>Front-end Web Developer</p>
+            <div className={`info ${isActive2 ? "active" : ""}`}>
+              <i className="fa fa-info fa-1x block" onClick={toggleActive2}></i>
+              <i
+                className="fa fa-angle-down fa-2x block"
+                onClick={toggleActive2}
+              ></i>
+              <p>
+                In short, I spend my earthly time striving to create some darned
+                awesome UI designs, and also develop a few. Student @{" "}
+               <a href="https://www.iitrpr.ac.in/" target="_blank">IIT Ropar
+                </a>
+                .
+              </p>
+              <p>Find me on:</p>
+              <a
+                className="link"
+                href="https://twitter.com/Masquetina/"
+                target="_blank"
+              >
+                <i className="fa fa-twitter fa-2x social"></i>
+              </a>
+              <a
+                className="link"
+                href="http://dribbble.com/Masquetina/"
+                target="_blank"
+              >
+                <i className="fa fa-dribbble fa-2x social"></i>
+              </a>
+              <a
+                className="link"
+                href="https://codepen.io/Masquetina"
+                target="_blank"
+              >
+                <i className="fa fa-codepen fa-2x social"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div id="card">
+          <div id="blur">
+            <div id="color"></div>
+          </div>
+          <div id="profile">
+            <img
+              src="Shashank.png"
+              alt="User"
+            />
+            <h1>Shashank Kumar</h1>
+            <p> +91 7764833505</p>
+            <p>Front-end Web Developer</p>
+            <div className={`info ${isActive3 ? "active" : ""}`}>
+              <i className="fa fa-info fa-1x block" onClick={toggleActive3}></i>
+              <i
+                className="fa fa-angle-down fa-2x block"
+                onClick={toggleActive3}
+              ></i>
+              <p>
+                In short, I spend my earthly time striving to create some darned
+                awesome UI designs, and also develop a few. Student @{" "}
+               <a href="https://www.iitrpr.ac.in/" target="_blank">IIT Ropar
+                </a>
+                .
+              </p>
+              <p>Find me on:</p>
+              <a
+                className="link"
+                href="https://twitter.com/Masquetina/"
+                target="_blank"
+              >
+                <i className="fa fa-twitter fa-2x social"></i>
+              </a>
+              <a
+                className="link"
+                href="http://dribbble.com/Masquetina/"
+                target="_blank"
+              >
+                <i className="fa fa-dribbble fa-2x social"></i>
+              </a>
+              <a
+                className="link"
+                href="https://codepen.io/Masquetina"
+                target="_blank"
+              >
+                <i className="fa fa-codepen fa-2x social"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+        </div>
+
+
+       
+      </div>
+    </div>
+
+
+    </div>
+  );
+};
+
+export default AboutUs;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,55 +1,79 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div className='flex flex-col items-center'>
-        <img src='iitrpr.jpg' alt='Homepage' className='w-3000 h-3000 mt-4 mb-8' />
-        <br></br>
-        <br></br>
-        <br></br>
-        <p className='text-8xl text-black mt-8 mb-10'>Welcome to our Portal!</p>
-        <p className='text-5xl text-black mt-5 mb-5'>This portal is designed to shift the current on-paper process to a complete online mode.</p>
-        <p className='text-5xl text-black mt-5'><Link to='/AboutUs'className='text-blue-600'>To learn more, visit our AboutUs section.</Link></p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <div className='w-full max-w-4xl mt-8'>
-            <h2 className='text-8xl text-black mt-8 mb-10'>Teams</h2>
-            <div className='flex flex-wrap justify-center'>
-                <div className='w-100 h-100 m-20'>
-                    <img src='Doodhnath.jpeg' alt='Doodhnath Tiwari' className='w-64 h-64 rounded-full' />
-                    <summary className='text-3xl text-black-700 text-center mt-2'>Doodhnath Tiwari</summary>
-                    <p className='text-3xl text-black-700 text-center mt-2'>+91 6203087513</p>
-                </div>
-                <div className='w-60 h-60 m-20'>
-                    <img src='Divyankar.jpeg' alt='Divyankar Shah' className='w-64 h-64 rounded-full' />
-                    <summary className='text-3xl text-black-700 text-center mt-2'>Divyankar Shah</summary>
-                    <p className='text-3xl text-black-700 text-center mt-2'>+91 9569691304</p>
-                </div>
-                <div className='w-60 h-60 m-20'>
-                    <img src='Niroopma.jpg' alt='Niroopma Verma' className='w-64 h-64 rounded-full' />
-                    <summary className='text-3xl text-black-700 text-center mt-2'>Niroopma Verma</summary>
-                    <p className='text-3xl text-black-700 text-center mt-2'>+91 7705834401</p>
-                </div>
-                <hr></hr>
-                <div className='w-60 h-60 m-20'>
-                    <img src='Shashank.png' alt='Shashank Kumar' className='w-64 h-64 rounded-full' />
-                    <summary className='text-3xl text-black-700 text-center mt-2'>Shashank Kumar</summary>
-                    <p className='text-3xl text-black-700 text-center mt-2'>+91 7764833505</p>
-                </div>
-            </div>
-        </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <div className='w-full max-w-4xl mt-8'>
-            <p className='text-6xl text-black mt-8 mb-10'>Have some questions?</p>
-            <p className='text-6xl text-black mt-8 mb=10'><Link to='/FAQ'className='text-blue-600'>Visit our Help Center</Link></p>
-        </div>
-    </div>
-  )
-}
+    <div className="home">
+      <img
+        src="iitrpr.jpg"
+        alt="Homepage"
+        className=" homepage-image w-3000 h-3000 mt-4 "
+      />
+     
+      <div className="home-main flex-col text-center">
+        <p className="text-6xl  text-black mt-8 mb-10">
+          Welcome to{" "}
+          <div className="brown-rang">Purchase Management Portal! </div>
+        </p>
+        <p className="text-3xl text-black  ml-7 mr-7 mt-2 mb-5">
+          At IIT Ropar, we believe in efficient and transparent procurement
+          processes to meet the needs of our academic community. Our Purchase
+          Management System streamlines the purchasing workflow, ensuring smooth
+          transactions and optimal resource allocation.
+        </p>
+        <p className=" font-bold text-left heading-main-body  mt-13">Features of Our Purchase Management System:</p>
 
-export default HomePage
+        <div className="features-main-body">
+      <div className="feature">
+        <div>User-Friendly Interface</div>
+        <p>Our intuitive interface makes it easy for faculty, staff, and administrators to initiate, track, and manage purchase requests.</p>
+      </div>
+      <div className="feature">
+        <h2>Centralized Procurement</h2>
+        <p>Consolidate all purchasing activities in one platform, enhancing coordination and reducing administrative overhead.</p>
+      </div>
+      <div className="feature">
+        <h2>Transparent Approval Workflow</h2>
+        <p>Clear approval workflows ensure accountability and compliance with institutional policies and regulations.</p>
+      </div>
+      <div className="feature">
+        <h2>Vendor Management</h2>
+        <p>Maintain a comprehensive database of trusted vendors, simplifying the procurement process and fostering partnerships.</p>
+      </div>
+      <div className="feature">
+        <h2>Budget Tracking</h2>
+        <p>Track expenditures in real-time to effectively manage budgets and prevent overspending.</p>
+      </div>
+      <div className="feature">
+        <h2>Efficient Communication</h2>
+        <p>Facilitate seamless communication between requesters, approvers, and vendors, minimizing delays and errors.</p>
+      </div>
+    </div>
+      </div>
+
+      <p className="text-5xl text-black mt-5">
+        <Link to="/AboutUs" className="text-blue-600">
+          To learn more, visit our AboutUs section.
+        </Link>
+      </p>
+      <br></br>
+      <br></br>
+      <br></br>
+    
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="w-full max-w-4xl mt-8">
+        <p className="text-6xl text-black mt-8 mb-10">Have some questions?</p>
+        <p className="text-6xl text-black mt-8 mb=10">
+          <Link to="/FAQ" className="text-blue-600 hover:bg-blue-gray-700">
+            Visit our Help Center
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
